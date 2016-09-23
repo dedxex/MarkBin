db14c06d483240e83425037e3a634d5778d4b294
Taranjeet singh <taranjeetplay@gmail.com>
2016-09-23 23:21:35 +0530

    removed a bug

--- client/components/bins/bins_share.js
+++ client/components/bins/bins_share.js
@@ -1,16 +1,17 @@
 import React,{ Component } from 'react';
 class BinsShare extends Component {
-    onShareClick() {
-        const email = this.refs.email.value;
-    }
+    
     render() {
+        const onShareClick = () => {
+            console.log("shared");
+        };
         return (
             <footer className="bins-share">
                 <div className="input-group">
-                    <input refs="email" className="form-control" />
+                    <input  className="form-control" />
                     <div className="input-group-btn">
                         <button 
-                            onClick={onShareClick.bind(this)}
+                            onClick={onShareClick}
                             className="btn btn-default">
                             ShareBin
                         </button>
